dacdbec62afe64a22118bfbf97e27a154d03a652
morantg <bernanosg@gmail.com>
2020-02-03 11:39:12 +0100

    ajout d'une méthode show pour le client avec sécurité si le client n'appartient pas à l'utilisateur

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Client;
 use App\Repository\ClientRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,7 +11,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ClientController extends AbstractController
 {
-     /**
+    /**
+     * @Route("/api/clients/{id}", name="app_clients_show", methods={"GET"})
+     */
+    public function showAction(Client $client, ClientRepository $clientRepository, Request $request, UserInterface $user)
+    {
+        
+        if($user != $client->getUser()){
+            return $this->json([
+                'status' => 401,
+                'message' => "Acces Denied"
+            ], 401);
+        }
+        return $this->json($client, 200, [], ['groups' => 'client:read']);
+    }
+    
+    /**
      * @Route("/api/clients", name="app_clients_list", methods={"GET"})
      */
     public function listAction(ClientRepository $clientRepository, Request $request, UserInterface $user)
