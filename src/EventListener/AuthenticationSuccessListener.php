d83444d22963a95127db044726cae5a539f7fd3f
morantg <bernanosg@gmail.com>
2020-02-07 11:08:29 +0100

    ajout d'un AuthenticationSuccessEvent pour renvoyer des infos de l'utilisateur lors de la connexion

---
+++ src/EventListener/AuthenticationSuccessListener.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\EventListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+
+class AuthenticationSuccessListener{
+
+    /**
+     * @param AuthenticationSuccessEvent $event
+     */
+    public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
+    {
+        $data = $event->getData();
+        $user = $event->getUser();
+        
+        $data['data'] = array(
+            'roles' => $user->getRoles(),
+            'email' => $user->getEmail(),
+        );
+
+        $event->setData($data);
+    }
+}
